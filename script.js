096681e4ce46d9617e9a4eefac1d486aafa1316e
findCarolinaCosta <carolinadacosta1997@gmail.com>
2021-09-28 00:38:29 -0300

    Adiciona arquivos desenvolvidos do projeto ate entao

---
+++ script.js
@@ -0,0 +1,43 @@
+const inputEmail = document.getElementById('input-email-header');
+const inputSenha = document.getElementById('input-senha');
+const botaoEntrar = document.getElementById('botao-entrar');
+
+function validarLogin() {
+  if (inputEmail.value === 'tryber@teste.com' && inputSenha.value === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
+}
+
+botaoEntrar.addEventListener('click', validarLogin);
+
+const buttonSubmit = document.getElementById('submit-btn');
+const checkboxAgreement = document.getElementById('agreement');
+
+function habilitaSubmit() {
+  const checkboxAgreementChecked = document.getElementById('agreement').checked;
+  if (checkboxAgreementChecked === false) {
+    buttonSubmit.disabled = true;
+  } else {
+    buttonSubmit.disabled = false;
+  }
+}
+
+buttonSubmit.disabled = true;
+checkboxAgreement.addEventListener('click', habilitaSubmit);
+
+const textarea = document.querySelector('textarea');
+function countCaractere(event) {
+  let count = document.getElementById('counter');
+  const current = event.currentTarget;
+  const maxLength = current.getAttribute('maxlength');
+  const currentLength = current.value.length;
+
+  if (currentLength >= maxLength) {
+    alert('Atingido o máximo de caractere');
+  }
+  count = maxLength - currentLength;
+  document.getElementById('counter').innerText = count;
+}
+textarea.addEventListener('input', countCaractere);
